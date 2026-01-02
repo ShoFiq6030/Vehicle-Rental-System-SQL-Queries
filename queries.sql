2502e5fcc883d0bd36ece78835229dc852ebd071
shofiq6030 <mdshofiq6030@gmail.com>
2026-01-02 23:49:21 +0600

    update- sql file

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ SELECT b.booking_id,
       b.start_date,
       b.end_date,
       b.status
-      FROM booking b JOIN users u ON b.user_id = u.user_id JOIN vehicles v ON b.vehicle_id=v.vehicle_id
+      FROM bookings b JOIN users u ON b.user_id = u.user_id JOIN vehicles v ON b.vehicle_id=v.vehicle_id
 
 
 -- Query 2: EXISTS
@@ -17,7 +17,7 @@ SELECT *
 FROM Vehicles v
 WHERE NOT EXISTS (
   SELECT b.vehicle_id
-    FROM booking b
+    FROM bookings b
     WHERE b.vehicle_id = v.vehicle_id
 );
 
@@ -36,7 +36,7 @@ SELECT
     v.name,
     COUNT(b.booking_id) AS total_bookings
 FROM vehicles v 
-JOIN booking b ON v.vehicle_id = b.vehicle_id
+JOIN bookings b ON v.vehicle_id = b.vehicle_id
 GROUP BY v.vehicle_id, v.name
 HAVING COUNT(b.booking_id) > 2
 
